935e90ff670c0fa4444454951ad9676028ea56b1
nicollasdev <contatonicollasfeitosa@gmail.com>
2021-09-16 16:04:52 -0300

    Add server command to Go Discover

--- go-discover/app/app.go
+++ go-discover/app/app.go
@@ -14,17 +14,25 @@ func Gerar() *cli.App {
 	app.Name = "Go Discover"
 	app.Usage = "Busca IPs e hostnames de servidores na internet"
 	
+	flags := []cli.Flag {
+		cli.StringFlag{
+			Name: "host",
+			Value: "nicollas.dev",
+		},
+	}
+
 	app.Commands = []cli.Command { // Slice de structures do tipo cli.Command
 		{
 			Name: "ip",
 			Usage: "Busca IPs de endereços na internet",
-			Flags: []cli.Flag {
-				cli.StringFlag{
-					Name: "host",
-					Value: "nicollas.dev",
-				},
-			},
+			Flags: flags,
 			Action: buscarIps,
+		},
+		{
+			Name: "server",
+			Usage: "Busca o nome de servidores na internet",
+			Flags: flags,
+			Action: buscarServidores,
 		},
 	}
 
@@ -43,5 +51,18 @@ func buscarIps(c *cli.Context)  {
 
 	for _, ip := range ips {
 		fmt.Println(ip);
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servers, erro := net.LookupNS(host) // name-servers
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, server := range servers {
+		fmt.Println(server.Host);
 	}
 }
